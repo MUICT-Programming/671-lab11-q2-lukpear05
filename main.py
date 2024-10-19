dbd112021d48e4d30ddb67bec6b27b8eeeee42dc
lukpear05 <nitchakan.soo@student.mahidol.edu>
2024-10-19 20:19:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 # YOUR CODE HERE
+def summation(lst1, lst2):
+    updated_list = [lst1[i] + lst2[i] for i in range(len(lst1))]
+    return updated_list
+
+def find_min_max(lst):
+    return (min(lst), max(lst))
+
+n = int(input())
+
+lst1 = []
+
+for i in range(n):
+    lst1.append(int(input()))
+
+lst2 = []
+
+for i in range(n):
+    lst2.append(int(input()))
+
+updated_list = summation(lst1, lst2)
+
+print(updated_list)
+
+min_max = find_min_max(updated_list)
+
+print(min_max)
